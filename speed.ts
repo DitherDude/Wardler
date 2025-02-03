01d669754e8222500373a01328984a21f3834141
Dither Dude <88754523+DitherDude@users.noreply.github.com>
2025-02-03 22:16:08 +1100

    Thanks @Term164 ! Big update coming soon!

---
+++ speed.ts
@@ -0,0 +1,14 @@
+export function calculateGroundVehicleSpeed(model: VehicleModel): number {
+  const data = model.VehiclePhys;
+
+  return Math.round(
+    ((data.engine.maxRPM * data.mechanics.driveGearRadius) /
+      (data.mechanics.mainGearRatio *
+        data.mechanics.sideGearRatio *
+        data.mechanics.gearRatios.ratio[
+          data.mechanics.gearRatios.ratio.length - 1
+        ])) *
+      0.12 *
+      Math.PI,
+  );
+}
